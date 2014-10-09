04e91027316fa1bb2448fba22e3850a01f962289
Martin Haas <maege86@gmail.com>
2014-10-09 17:50:53 +0200

    Add smaller devices.

--- Praktikum04/js/domscript.js
+++ Praktikum04/js/domscript.js
@@ -1,4 +1,9 @@
 function dom_init() {
+    $("a.menu").click(function() {
+        $("body").animate({ scrollTop: $(document).height()}, "fast");
+    });
 
-  
+    $("li.top a").click(function() {
+        $("body").animate({ scrollTop: 0}, "fast");
+    });
 }
