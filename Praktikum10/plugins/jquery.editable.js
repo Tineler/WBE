c55a7b96db15a8543cb0648b20d7a19b58bb3700
Martin Haas <maege86@gmail.com>
2014-11-23 00:32:58 +0100

    Replacement of elements.

--- Praktikum10/plugins/jquery.editable.js
+++ Praktikum10/plugins/jquery.editable.js
@@ -1,8 +1,33 @@
 (function($) {
+    "use strict";
+    $.fn.editable = function(options) {
 
-    $.fn.editable = function() {
+        var options = $.extend({
+            save: function() {}
+        }, options || {});
+
+        var switchToP = function() {
+            var $p = $("<p>", {
+                class: "editable",
+                html: $(this).val().trim().replace(/\n/g,"<br>")
+        });
+            $(this).replaceWith($p);
+            $p.on("click", switchToInput);
+        };
+
+        var switchToInput = function() {
+            var $input = $("<textarea>", {
+                val: $(this).html().trim().replace(/<br>/g, "\n")
+            });
+            $(this).replaceWith($input);
+            $input.focus();
+            $input.on("blur", switchToP);
+        };
+
         return this.each(function() {
-            $(this).attr('contenteditable','true');
+            if(this.tagName == "P") {
+                $(this).on("click", switchToInput);
+            }
         });
     }
 
