a18cc74635698c5e857508cbba0f72a479b9d5e6
Martin Haas <maege86@gmail.com>
2014-11-23 11:19:05 +0100

    Added save callback.

--- Praktikum10/plugins/jquery.editable.js
+++ Praktikum10/plugins/jquery.editable.js
@@ -3,25 +3,52 @@
     $.fn.editable = function(options) {
 
         var options = $.extend({
-            save: function() {}
+            save: function(el) {}
         }, options || {});
 
-        var switchToP = function() {
+        var onBlur = function(element, value) {
+
+            if (value == null) {
+                value = $(element).val().trim().replace(/\n/g,"<br>");
+            }
             var $p = $("<p>", {
                 class: "editable",
-                html: $(this).val().trim().replace(/\n/g,"<br>")
-        });
-            $(this).replaceWith($p);
+                html: value
+            });
+            $(element).replaceWith($p);
+            while($p.next("button").length > 0) {
+                $p.next("button").remove();
+            }
             $p.on("click", switchToInput);
         };
 
         var switchToInput = function() {
+            var oldValue = $(this).html().trim();
             var $input = $("<textarea>", {
-                val: $(this).html().trim().replace(/<br>/g, "\n")
+                val: oldValue.replace(/<br>/g, "\n")
             });
             $(this).replaceWith($input);
+            $input.next("a").remove();
             $input.focus();
-            $input.on("blur", switchToP);
+
+            var $buttonCancel = $("<button>", {
+                text: "Cancel",
+                class: "cancel"
+            });
+            $input.after($buttonCancel);
+            $buttonCancel.click(function() {
+                onBlur($input[0], oldValue);
+            });
+
+            var $buttonSave = $("<button>", {
+                text: "Save",
+                class: "save"
+            });
+            $input.after($buttonSave);
+            $buttonSave.click(function() {
+                options.save($input[0]);
+                onBlur($input[0]);
+            });
         };
 
         return this.each(function() {
